56ad1cb14ad40ef73974a979674fc3f009de82ec
artemadam <38755323+artemadam@users.noreply.github.com>
2021-09-22 16:37:15 +0200

    Add files via upload

--- pose_estimation.py
+++ pose_estimation.py
@@ -1,97 +1,170 @@
-import cv2
 import mediapipe as mp
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 
-mpDraw = mp.solutions.drawing_utils
-mpPose = mp.solutions.pose
-pose = mpPose.Pose()
-
-cap = cv2.VideoCapture(0)
-
-boxes = []
-
-def getBoundingbox(list):
-    x=[]
-    y=[]
-    for lm in list:
-        x.append(lm[1])
-        y.append(lm[2])
-
-    top = min(y)
-    bot = max(y)
-    left = min(x)
-    right = max(x)
-    start = (left, top)
-    end = (right, bot)
-    return start, end
+######### init mediapipe solution ##################
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+pose = mp_pose.Pose(
+                    static_image_mode=False,
+                    model_complexity=1,
+                    enable_segmentation=False,
+                    min_detection_confidence=0.5)
 
 
-def getList(results, img):
-    lm_list = []
+####################################################
 
-    for id, lm in enumerate(results.pose_landmarks.landmark):
-        h, w, c = img.shape
-        x, y = int(lm.x * w), int(lm.y * h)
-        if lm.visibility>0.5:
-            lm_list.append([id, x, y, lm.visibility])
-
-    return lm_list
+#################### fall detection functions ###########################################
+def inference(frame):
+    # run mediapipe net on frame and output results
+    results = pose.process(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+    return results
 
 
-def fallen(start, end):
-    height = end[1]-start[1]
-    width = end[0]-start[0]
-    fallen = False
-    if height<width:
-        fallen = True
-
-    return fallen
-
-
-def save_boxes(start, end):
-    f = fallen(start, end)
-    boxes.append([start, end, f])
+def results_to_keypoints(frame, results):
+    # output: numpy array with shape=[33,3] 33 keypoints, x,y and z
+    h, w, c = frame.shape
+    keypoints = np.zeros((33, 3))
+    landmark_list = results.pose_landmarks.landmark
+    for i, landmark in enumerate(landmark_list):
+        keypoints[i, 0] = landmark.x * w
+        keypoints[i, 1] = landmark.y * h
+        keypoints[i, 2] = landmark.z * w
+    return keypoints
 
 
-def detect_fall(start, end):
-    counter = 0
-    fall = False
-
-    for box in boxes[-9:]:
-        if box[2]:
-            counter += 1
-
-    if counter>8:
-        fall = True
-
-    return fall
+def find_human_axis(keypoints):
+    #axis of upper body. from hip to shoulders
+    v1 = keypoints[12] - keypoints[24]
+    v2 = keypoints[11] - keypoints[23]
+    return (v1 + v2) / 2
 
 
+def calibrate_human_axis():
+    # have live webcam feed. stand upright and press q. the reference orientation(3d vector) of 'upright' is saved
+    cap = cv.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if (not success):
+            print('webcam not found')
+            break
+        
+        results = inference(frame)
+        if results.pose_landmarks is not None:
+            plot_landmarks(frame, results)
+            keypoints = results_to_keypoints(frame, results)
+            reference_human_axis = find_human_axis(keypoints)
+            color = (0, 255, 0)
+            plot_line(frame, keypoints, reference_human_axis, color)
+        
+        cv.imshow('live', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+    cv.waitKey(1)
+    return reference_human_axis
+
+
+def fall_detection_human_axis(current_axis, reference_axis):
+    #if current orientation is dissimiliar to calibrated reference oreintation -> fall detection
+    if current_axis is None or reference_axis is None:
+        return False
+    similiarity = np.dot(current_axis, reference_axis) / (np.linalg.norm(current_axis) * np.linalg.norm(reference_axis))
+    if similiarity < 0.5:
+        return True
+    else:
+        return False
+
+
+################################################################################################
+
+############ helper functions plotting #########################################################################
+def plot_landmarks(frame, results):
+    mp_drawing.draw_landmarks(
+                              frame,
+                              results.pose_landmarks,
+                              mp_pose.POSE_CONNECTIONS,
+                              landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
+                              )
+
+
+def plot_text(frame, text, color):
+    font = cv.FONT_HERSHEY_SIMPLEX
+    position = (50, 50)
+    fontScale = 1
+    thickness = 2
+    cv.putText(frame, text, position, font, fontScale, color, thickness, cv.LINE_AA)
+
+
+def plot_line(frame, keypoints, human_axis, color):
+    startx = int(0.5 * (keypoints[24, 0] + keypoints[23, 0]))
+    starty = int(0.5 * (keypoints[24, 1] + keypoints[23, 1]))
+    start = (startx, starty)
+    endx = int(startx + human_axis[0])
+    endy = int(starty + human_axis[1])
+    end = (endx, endy)
+    thickness = 2
+    cv.line(frame, start, end, color, thickness)
+
+
+def plot_both_axes(current_human_axis, reference_human_axis):
+    #3d matplotlib vector plot of reference axis and current axis
+    cha = current_human_axis / np.linalg.norm(current_human_axis)
+    rha = reference_human_axis / np.linalg.norm(reference_human_axis)
+    ax.cla()
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(-1, 1)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    # ax.view_init(elev=0, azim=270)
+    ax.quiver(0, 0, 0, cha[0], cha[1], cha[2])
+    ax.quiver(0, 0, 0, rha[0], rha[1], rha[2])
+    plt.pause(0.0001)
+
+
+##################################################################################################
+
+
+###################################### main loop ##################################################
+reference_human_axis = calibrate_human_axis()
+
+cap = cv.VideoCapture(0)
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    success, frame = cap.read()
+    if (not success):
+        print('webcam not found')
+        continue
+    results = inference(frame)
+    if results.pose_landmarks is not None:
+        plot_landmarks(frame, results)
+        keypoints = results_to_keypoints(frame, results)
+        current_human_axis = find_human_axis(keypoints)
+        
+        detected = fall_detection_human_axis(current_human_axis, reference_human_axis)
+        if detected:
+            color = (0, 0, 255)
+            text = 'fall'
+        else:
+            color = (0, 255, 0)
+            text = 'not fall'
+        plot_text(frame, text, color)
+        plot_line(frame, keypoints, current_human_axis, color)
 
-    results = pose.process(imgRGB)
+    cv.imshow('live', frame)
 
-    if results.pose_landmarks:
-        list = getList(results, img)
-        start, end = getBoundingbox(list)
-        if len(list) > 1:
-            if detect_fall(start,end):
-                text = "Gefallen"
-            else:
-                text = "Steht noch"
-            save_boxes(start, end)
-            if boxes[-1][2]:
-                color = (0,0,255)
-            else:
-                color = (0,255,0)
-            img = cv2.rectangle(img, start, end, color, thickness=3)
-            img = cv2.putText(img, text, (20, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
-
-        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-
-
-
+    if not (current_human_axis is None):
+        plot_both_axes(current_human_axis, reference_human_axis)
+        
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
+cv.waitKey(1)
