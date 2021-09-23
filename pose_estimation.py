b49a6ebb6334f769689b929f3aa64007a8248c8e
artemadam <38755323+artemadam@users.noreply.github.com>
2021-09-23 16:02:10 +0200

    fall detection based on orientation of upper_body, left_leg, right_leg

--- pose_estimation.py
+++ pose_estimation.py
@@ -1,7 +1,6 @@
 import mediapipe as mp
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 
 ######### init mediapipe solution ##################
 mp_pose = mp.solutions.pose
@@ -35,46 +34,30 @@ def results_to_keypoints(frame, results):
     return keypoints
 
 
-def find_human_axis(keypoints):
-    #axis of upper body. from hip to shoulders
+def find_upper_body(keypoints):
     v1 = keypoints[12] - keypoints[24]
     v2 = keypoints[11] - keypoints[23]
-    return (v1 + v2) / 2
+    return (v1+v2)/2
 
+def find_left_leg(keypoints):
+    return keypoints[23] - keypoints[27]
 
-def calibrate_human_axis():
-    # have live webcam feed. stand upright and press q. the reference orientation(3d vector) of 'upright' is saved
-    cap = cv.VideoCapture(0)
-    while True:
-        success, frame = cap.read()
-        if (not success):
-            print('webcam not found')
-            break
-        
-        results = inference(frame)
-        if results.pose_landmarks is not None:
-            plot_landmarks(frame, results)
-            keypoints = results_to_keypoints(frame, results)
-            reference_human_axis = find_human_axis(keypoints)
-            color = (0, 255, 0)
-            plot_line(frame, keypoints, reference_human_axis, color)
-        
-        cv.imshow('live', frame)
-        if cv.waitKey(1) == ord('q'):
-            break
+def find_right_leg(keypoints):
+    return keypoints[24] - keypoints[28]
 
-    cap.release()
-    cv.destroyAllWindows()
-    cv.waitKey(1)
-    return reference_human_axis
+def find_orientation(line):
+    orientation = -line[1] / np.abs(line[0])
+    return orientation
 
-
-def fall_detection_human_axis(current_axis, reference_axis):
-    #if current orientation is dissimiliar to calibrated reference oreintation -> fall detection
-    if current_axis is None or reference_axis is None:
-        return False
-    similiarity = np.dot(current_axis, reference_axis) / (np.linalg.norm(current_axis) * np.linalg.norm(reference_axis))
-    if similiarity < 0.5:
+def fall_detection_lines(upper_body_is_not_upright, left_leg_is_not_upright, right_leg_is_not_upright):
+    fall_votes = 0
+    if upper_body_is_not_upright:
+        fall_votes += 1
+    if left_leg_is_not_upright:
+        fall_votes += 1
+    if right_leg_is_not_upright:
+        fall_votes += 1
+    if fall_votes >= 2:
         return True
     else:
         return False
@@ -100,43 +83,45 @@ def plot_text(frame, text, color):
     cv.putText(frame, text, position, font, fontScale, color, thickness, cv.LINE_AA)
 
 
-def plot_line(frame, keypoints, human_axis, color):
+def plot_upper_body(frame, keypoints, vector, color):
     startx = int(0.5 * (keypoints[24, 0] + keypoints[23, 0]))
     starty = int(0.5 * (keypoints[24, 1] + keypoints[23, 1]))
     start = (startx, starty)
-    endx = int(startx + human_axis[0])
-    endy = int(starty + human_axis[1])
+    endx = int(startx + vector[0])
+    endy = int(starty + vector[1])
     end = (endx, endy)
     thickness = 2
     cv.line(frame, start, end, color, thickness)
 
 
-def plot_both_axes(current_human_axis, reference_human_axis):
-    #3d matplotlib vector plot of reference axis and current axis
-    cha = current_human_axis / np.linalg.norm(current_human_axis)
-    rha = reference_human_axis / np.linalg.norm(reference_human_axis)
-    ax.cla()
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
-    ax.set_zlim(-1, 1)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    # ax.view_init(elev=0, azim=270)
-    ax.quiver(0, 0, 0, cha[0], cha[1], cha[2])
-    ax.quiver(0, 0, 0, rha[0], rha[1], rha[2])
-    plt.pause(0.0001)
+def plot_left_leg(frame, keypoints, vector, color):
+    startx = int(keypoints[27, 0])
+    starty = int(keypoints[27, 1])
+    start = (startx, starty)
+    endx = int(startx + vector[0])
+    endy = int(starty + vector[1])
+    end = (endx, endy)
+    thickness = 2
+    cv.line(frame, start, end, color, thickness)
+
+
+def plot_right_leg(frame, keypoints, vector, color):
+    startx = int(keypoints[28, 0])
+    starty = int(keypoints[28, 1])
+    start = (startx, starty)
+    endx = int(startx + vector[0])
+    endy = int(starty + vector[1])
+    end = (endx, endy)
+    thickness = 2
+    cv.line(frame, start, end, color, thickness)
 
 
 ##################################################################################################
 
 
 ###################################### main loop ##################################################
-reference_human_axis = calibrate_human_axis()
 
 cap = cv.VideoCapture(0)
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
 while True:
     success, frame = cap.read()
     if (not success):
@@ -146,25 +131,51 @@ while True:
     if results.pose_landmarks is not None:
         plot_landmarks(frame, results)
         keypoints = results_to_keypoints(frame, results)
-        current_human_axis = find_human_axis(keypoints)
+        upper_body = find_upper_body(keypoints)
+        left_leg = find_left_leg(keypoints)
+        right_leg = find_right_leg(keypoints)
         
-        detected = fall_detection_human_axis(current_human_axis, reference_human_axis)
-        if detected:
+        upper_body_orientation = find_orientation(upper_body)
+        left_leg_orientation = find_orientation(left_leg)
+        right_leg_orientation = find_orientation(right_leg)
+        
+        upper_body_is_not_upright = upper_body_orientation < 1
+        if upper_body_is_not_upright:
             color = (0, 0, 255)
-            text = 'fall'
         else:
             color = (0, 255, 0)
-            text = 'not fall'
+        plot_upper_body(frame, keypoints, upper_body, color)
+
+        left_leg_is_not_upright = left_leg_orientation < 1
+        if left_leg_is_not_upright:
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
+        plot_left_leg(frame, keypoints, left_leg, color)
+        
+        right_leg_is_not_upright = right_leg_orientation < 1
+        if right_leg_is_not_upright:
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
+        plot_right_leg(frame, keypoints, right_leg, color)
+        
+        
+        fall_detected = fall_detection_lines(upper_body_is_not_upright, left_leg_is_not_upright, right_leg_is_not_upright)
+        if fall_detected:
+            color = (0, 0, 255)
+            text = 'Fall'
+        else:
+            color = (0, 255, 0)
+            text = 'Kein Fall'
         plot_text(frame, text, color)
-        plot_line(frame, keypoints, current_human_axis, color)
+
 
     cv.imshow('live', frame)
 
-    if not (current_human_axis is None):
-        plot_both_axes(current_human_axis, reference_human_axis)
-        
     if cv.waitKey(1) == ord('q'):
         break
 cap.release()
 cv.destroyAllWindows()
 cv.waitKey(1)
+
