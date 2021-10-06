9c89a9a9895b34f2673d2e4452d4913e1d0f08c1
artemadam <38755323+artemadam@users.noreply.github.com>
2021-10-06 14:32:57 +0200

    Add files via upload

--- pose_estimation.py
+++ pose_estimation.py
@@ -2,19 +2,68 @@ import socket
 import json
 import time
 import multiprocessing
+import select
 
 from collections import deque
 
 import cv2
 import mediapipe as mp
 import numpy as np
+from numpy.linalg import inv
+
+##### Kalman #################################################################################
+
+class KalmanFilter:
+    def __init__(self, fall_value, confidence):
+        velocity = 0
+        self.x = np.array([fall_value, velocity])
+        self.P = np.array([
+                           [1/(confidence+1e-6),0],
+                           [0, 1000]
+                           ])
+        self.prev_time_stamp = time.time()
+    
+    def iterate(self, new_fall_value, new_confidence):
+        #prediction
+        next_time_stamp = time.time()
+        delta_t = next_time_stamp - self.prev_time_stamp
+        F = np.array([
+                      [1, delta_t],
+                      [0, 1]
+                      ])
+        #low sigma->high inertia
+        Sigma = 0.9
+        D = Sigma**2 * np.array([
+                                [1/4*delta_t**4, 1/2*delta_t**3],
+                                [1/2*delta_t**3, delta_t**2]
+                                ])
+        self.prev_time_stamp = next_time_stamp
+        self.x = F@self.x
+        self.P = F @ self.P @ F.T + D
+                      
+                      
+        #filtering
+        new_x = np.array([new_fall_value, 0])
+        new_P = np.array([
+                        [1/(new_confidence+1e-6), 0],
+                        [0, 1000]
+                        ])
+        filtered_P = inv(inv(self.P) + inv(new_P))
+        filtered_x = filtered_P@(inv(self.P)@self.x + inv(new_P)@new_x)
+                      
+        self.x = np.clip(filtered_x,0,1)
+        self.P = filtered_P
+
+##############################################################################################
 
 # global  ####################################################################################
 
 boxes = deque(10 * [[0, 0, 0]], 10)
 vid_detection_for_fusion = deque(10 * [[0.0, 0.0, 0.0]], 10)
 vid_detection = deque(10 * [[0.0, 0.0, 0.0]], 10)
-
+KF = KalmanFilter(0,0)
+fall_counter = 0
+counter_locked = False
 
 
 ##############################################################################################
@@ -64,39 +113,40 @@ def plot_right_leg(frame, keypoints, vector, color):
     cv2.line(frame, start, end, color, thickness)
 
 
-def setup_img(img_loc, start, end, lm_list, box_fall, lines_fall, lm_fall, fall_value):
+def setup_img(img_loc, start, end, lm_list, box_fall, lines_fall, lm_fall, fall_value, latest_fusion_values_loc):
+    global fall_counter
     if len(lm_list) > 1:
-        if box_fall > 0.5:
-            text = "Box: gefallen  " + str(box_fall)
+        """if box_fall > 0.5:
+            text = "Box: gefallen  " + str(round(box_fall,3))
             color = (0, 0, 255)
         else:
-            text = "Box: Steht noch  " + str(box_fall)
+            text = "Box: Steht noch  " + str(round(box_fall,3))
             color = (0, 255, 0)
         img_loc = plot_text(img_loc, text, color, (20, 30))
 
-        if lines_fall:
-            text = "Lines: gefallen  " + str(lines_fall)
+        if lines_fall > 0.5:
+            text = "Lines: gefallen  " + str(round(lines_fall,3))
             color = (0, 0, 255)
         else:
-            text = "Lines: Steht noch  " + str(lines_fall)
+            text = "Lines: Steht noch  " + str(round(lines_fall,3))
             color = (0, 255, 0)
         img_loc = plot_text(img_loc, text, color, (20, 60))
 
         if lm_fall > 0.5:
-            text = "LM: gefallen  " + str(lm_fall)
+            text = "LM: gefallen  " + str(round(lm_fall,3))
             color = (0, 0, 255)
         else:
-            text = "LM: Steht noch " + str(lm_fall)
+            text = "LM: Steht noch " + str(round(lm_fall,3))
             color = (0, 255, 0)
         img_loc = plot_text(img_loc, text, color, (20, 90))
 
         if fall_value > 0.5:
-            text = "Overall: gefallen  " + str(fall_value)
+            text = "Overall: gefallen  " + str(round(fall_value,3))
             color = (0, 0, 255)
         else:
-            text = "Overall: Steht noch " + str(fall_value)
+            text = "Overall: Steht noch " + str(round(fall_value,3))
             color = (0, 255, 0)
-        img_loc = plot_text(img_loc, text, color, (20, 120))
+        img_loc = plot_text(img_loc, text, color, (20, 120))"""
 
         if boxes[-1][2]:
             color = (0, 0, 255)
@@ -104,6 +154,31 @@ def setup_img(img_loc, start, end, lm_list, box_fall, lines_fall, lm_fall, fall_
             color = (0, 255, 0)
         img_loc = cv2.rectangle(img_loc, start, end, color, thickness=3)
         # img_loc = plot_text(img_loc, text, color, (20, 200))
+        
+        
+        video_fall, video_confidence, radar_fall, radar_confidence, fusion_fall, fusion_confidence = latest_fusion_values_loc
+        
+        video_text = "Video   Fall: " + str(round(video_fall, 2)) + "  Conf: " + str(round(video_confidence, 2))
+        radar_text = "Radar   Fall: " + str(round(radar_fall, 2)) + "    Conf: " + str(round(radar_confidence, 2))
+        fusion_text = "Fusion   Fall: " + str(round(fusion_fall, 2)) + "    Conf: " + str(round(fusion_confidence, 2))
+    
+        if video_fall > 0.5:
+            color = (0,0,255)
+        else:
+            color = (0,255,0)
+        plot_text(img_loc, video_text, color, (20,30))
+        if radar_fall > 0.5:
+            color = (0,0,255)
+        else:
+            color = (0,255,0)
+        plot_text(img_loc, radar_text, color, (20,60))
+        if fusion_fall > 0.5:
+            color = (0,0,255)
+        else:
+            color = (0,255,0)
+        plot_text(img_loc, fusion_text, color, (20,90))
+            
+        plot_text(img_loc, str(fall_counter), (255,255,255), (img_loc.shape[1] - 40, 30))
 
     return img_loc
 
@@ -113,14 +188,11 @@ def setup_img(img_loc, start, end, lm_list, box_fall, lines_fall, lm_fall, fall_
 # fall detection functions ###################################################################
 
 def calculate_fall_value(box_fall, lines_fall, lm_fall, box_weighting=1 / 3, lines_weighting=1 / 3, lm_weighting=1 / 3):
-    lines = 0.0
-    if lines_fall:
-        lines = 1.0
 
-    return box_fall * box_weighting + lines * lines_weighting + lm_fall * lm_weighting
+    return box_fall * box_weighting + lines_fall * lines_weighting + lm_fall * lm_weighting
 
 
-def start_detection(results_loc, img_loc):
+def start_detection(results_loc, img_loc, latest_fusion_values_loc):
     global boxes
     fall_value = 0
     if results_loc.pose_landmarks:
@@ -130,15 +202,17 @@ def start_detection(results_loc, img_loc):
         start, end = get_bounding_box(lm_list)
         box_fall = detect_box_fall(start, end)
 
-        # lines: Bool-Value  ###
-        lines_fall = detect_lines_fall(results_loc, img_loc)
+        # lines: Value [0,1]  ###
+        lines_fall = fall_detection_lines(results_loc, img_loc)
+        KF.iterate(lines_fall, 1)
+        lines_fall = KF.x[0]
 
         # landmarks: Value [0,1]  ###
         lm_fall = detect_lm_fall(lm_list, img_loc)
 
         fall_value = calculate_fall_value(box_fall, lines_fall, lm_fall)
 
-        img_loc = setup_img(img_loc, start, end, lm_list, box_fall, lines_fall, lm_fall, fall_value)
+        img_loc = setup_img(img_loc, start, end, lm_list, box_fall, lines_fall, lm_fall, fall_value, latest_fusion_values_loc)
         save_boxes(start, end)
     else:
         lm_list = []
@@ -191,6 +265,8 @@ def fallen(start, end):
     height = end[1] - start[1]
     width = end[0] - start[0]
     fallen = False
+    if width == 0:
+        width = 1
     if height / width < 1.3:
         fallen = True
 
@@ -245,57 +321,49 @@ def find_right_leg(keypoints):
 
 
 def find_orientation(line):
-    orientation = -line[1] / np.abs(line[0])
+    #scalar from [-1,1] -1 if upright, 0 if horizontal, 1 if upside down
+    orientation = np.arctan2(line[1] , np.abs(line[0])) / (np.pi/2)
     return orientation
 
 
-def fall_detection_lines(upper_body_is_not_upright, left_leg_is_not_upright, right_leg_is_not_upright):
-    fall_votes = 0
-    if upper_body_is_not_upright:
-        fall_votes += 1
-    if left_leg_is_not_upright:
-        fall_votes += 1
-    if right_leg_is_not_upright:
-        fall_votes += 1
-
-    if fall_votes >= 2:
-        return True
-    else:
-        return False
-
-
-def detect_lines_fall(results_loc, frame):
-    keypoints = results_to_keypoints(frame, results_loc)
+def fall_detection_lines(results, frame):
+    keypoints = results_to_keypoints(frame, results)
     upper_body = find_upper_body(keypoints)
     left_leg = find_left_leg(keypoints)
     right_leg = find_right_leg(keypoints)
-
+    
     upper_body_orientation = find_orientation(upper_body)
-    left_leg_orientation = find_orientation(left_leg)
-    right_leg_orientation = find_orientation(right_leg)
-
-    upper_body_is_not_upright = upper_body_orientation < 1
+    left_leg_orientation = np.clip(find_orientation(left_leg),-1,0.2)
+    right_leg_orientation = np.clip(find_orientation(right_leg),-1,0.2)
+    
+    upper_body_is_not_upright = upper_body_orientation > -0.5
     if upper_body_is_not_upright:
         color = (0, 0, 255)
     else:
         color = (0, 255, 0)
     plot_upper_body(frame, keypoints, upper_body, color)
 
-    left_leg_is_not_upright = left_leg_orientation < 1
+    left_leg_is_not_upright = left_leg_orientation > -0.5
     if left_leg_is_not_upright:
         color = (0, 0, 255)
     else:
         color = (0, 255, 0)
     plot_left_leg(frame, keypoints, left_leg, color)
-
-    right_leg_is_not_upright = right_leg_orientation < 1
+    
+    right_leg_is_not_upright = right_leg_orientation > -0.5
     if right_leg_is_not_upright:
         color = (0, 0, 255)
     else:
         color = (0, 255, 0)
     plot_right_leg(frame, keypoints, right_leg, color)
+    
+    
+    fall_value = (1/3)*(upper_body_orientation + left_leg_orientation + right_leg_orientation)
+    fall_value = fall_value+1
+    fall_value = np.clip(fall_value,0,1)
+    
+    return fall_value
 
-    return fall_detection_lines(upper_body_is_not_upright, left_leg_is_not_upright, right_leg_is_not_upright)
 
 
 # fall detection landmarks ###################################################################
@@ -350,18 +418,21 @@ def calculate_confidence(img_loc, lm_list):
 # main loop ######################################################################################
 
 
-def receive_per_udp(udp_ip, udp_port, radar_detection):
-    sock = socket.socket(socket.AF_INET,  # Internet
-                         socket.SOCK_DGRAM)  # UDP
-    sock.bind((udp_ip, udp_port))
-    while True:
-        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-        message_json = data.decode()  # this is just a string
+def receive_per_udp(sock):
+    timeout=0.01
+    inputs = [sock]
+    outputs = []
+    
+    readable, writable, exceptional = select.select(inputs, outputs, inputs, timeout)
+    if not readable:
+        return None
+    for readable_socket in readable:
+        data, addr = readable_socket.recvfrom(1024) # buffer size is 1024 bytes
+        message_json = data.decode()
         message_dict = json.loads(message_json)
-        radar_detection[0] = float(message_dict["time_stamp"])
-        radar_detection[1] = float(message_dict["fall_value"])
-        radar_detection[2] = float(message_dict["confidence"])
-        #print(radar_detection[0])
+        radar_detection = message_dict
+        radar_detection['timestamp'] = time.time()
+    return list(radar_detection.values())
 
 
 def append_new_fall_value():
@@ -377,16 +448,24 @@ def append_new_fall_value():
     vid_detection_for_fusion.append([now, fall / length_of_detection, confidence_loc / length_of_detection])
 
 
-def loop(radar_detection):
+def loop():
+    UDP_IP = '192.168.242.148'
+    #UDP_IP = '10.249.54.144'
+    UDP_PORT = 6789
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+    newest_radar_detection = [0,0,0] #initialize with 0 until acutal data arrives
+    
     mpDraw = mp.solutions.drawing_utils
     mpPose = mp.solutions.pose
     pose = mpPose.Pose()
     cap = cv2.VideoCapture(0)
-
-    c = 0
     current = time.time()
+    latest_fusion_values = 0, 0, 0, 0 ,0 ,0
 
     while True:
+        
         success, img = cap.read()
         if not success:
             print('webcam not found')
@@ -397,17 +476,22 @@ def loop(radar_detection):
         if results.pose_landmarks:
             mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
 
-        img, fall, confidence = start_detection(results, img)
+        img, fall, confidence = start_detection(results, img, latest_fusion_values)
         now = time.time()
         vid_detection.append([now, fall, confidence])
 
-        if time.time() - current >= 1:
+        
+        radar_detection = receive_per_udp(sock)
+        if radar_detection is not None:
+            newest_radar_detection = radar_detection
             append_new_fall_value()
-            fusion(radar_detection)
+            latest_fusion_values = fusion(newest_radar_detection, img)
             current = time.time()
 
+        update_counter(latest_fusion_values)
         cv2.imshow("Image", img)
-        cv2.waitKey(1)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
     cap.release()
     cv2.destroyAllWindows()
@@ -419,7 +503,7 @@ def loop(radar_detection):
 
 # fusion ######################################################################################
 
-def fusion(radar_detection):
+def fusion(radar_detection, img_loc):
     vid = vid_detection_for_fusion[-1]
     sum_of_confidences = 0
     fall_fusion = 0
@@ -427,27 +511,33 @@ def fusion(radar_detection):
     #print("rad: ", radar_detection[0])
     #print(abs(vid[0] - radar_detection[0]))
     # wenn die letzen einträge weniger als eine Sekunde auseinander liegen
-    if abs(vid[0] - radar_detection[0]) < 1:
-        sum_of_confidences = vid[2] + radar_detection[2]
-        if sum_of_confidences != 0:
-            g_vid = vid[2] / sum_of_confidences
-            g_radar = radar_detection[2] / sum_of_confidences
-            fall_fusion = g_vid * vid[1] + g_radar * radar_detection[1]
-    confidence_fusion = sum_of_confidences / 2
-    print(round(fall_fusion,2), round(confidence_fusion,2))
+    sum_of_confidences = vid[2] + radar_detection[2]
+    if sum_of_confidences != 0:
+        g_vid = vid[2] / sum_of_confidences
+        g_radar = radar_detection[2] / sum_of_confidences
+        fall_fusion = g_vid * vid[1] + g_radar * radar_detection[1]
+        confidence_fusion = sum_of_confidences / 2
+    
 
 
+        #print("Video   Fall: ", round(vid[1], 2), "  Conf: ", round(vid[2], 2))
+        #print("Radar   Fall: ", round(radar_detection[1], 2), "    Conf: ", round(radar_detection[2], 2))
+        #print("Fusion   Fall: ", round(fall_fusion, 2), "    Conf: ", round(confidence_fusion, 2))
+        #print()
+    return vid[1], vid[2]  , radar_detection[1], radar_detection[2], fall_fusion, confidence_fusion
+
+
+def update_counter(latest_fusion_values_loc):
+    global counter_locked, fall_counter
+    video_fall, video_confidence, radar_fall, radar_confidence, fusion_fall, fusion_confidence = latest_fusion_values_loc
+    if not counter_locked:
+        if fusion_fall > 0.5:
+            fall_counter += 1
+            counter_locked = True
+    else:
+        if fusion_fall < 0.1:
+            counter_locked = False
 
 if __name__ == "__main__":
-    UDP_IP = "localhost"
-    UDP_PORT = 6789
+    loop()
 
-    radar_detection = multiprocessing.Array('d', 3, lock=multiprocessing.Lock())
-
-    video = multiprocessing.Process(target=loop, args=[radar_detection])
-    udp = multiprocessing.Process(target=receive_per_udp, args=[UDP_IP, UDP_PORT, radar_detection])
-
-    video.start()
-    udp.start()
-
-    #loop(radar_detection)
